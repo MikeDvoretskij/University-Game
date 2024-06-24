caa30607ab5f3022355d709997b3e2231992df26
MikeDvoretskij <mixall2003@bk.ru>
2024-06-24 17:06:52 +0300

    -

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new();
+        return app})
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
